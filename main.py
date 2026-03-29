bf633604d84d1003c87d84250e6fc6952e58e195
mjy1427-wq <mjy1427@gmail.com>
2026-03-29 19:19:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import Updater, CallbackQueryHandler, CallbackContext, Message
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # --- [1. 기본 설정 및 관리자 정보] ---
-TOKEN = "8603959168:AAH9Jq_5erWZgvocsw" #
+TOKEN = "8603959168:AAH9Jq_5erWZgvocsvnjS1rP4G_F9VW-CbQ” #
 ADMIN_ID = 7476630349 #
 
 # 등급별 아이콘 설정 (이미지 UI 재현)
